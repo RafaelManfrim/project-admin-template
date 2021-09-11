00d38a553e98064bbff3d2be76d099497ee4df66
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-09-11 17:01:05 -0300

    Logout button style

--- src/components/template/Sidebar.tsx
+++ src/components/template/Sidebar.tsx
@@ -1,6 +1,6 @@
 import SidebarItem from './SidebarItem'
 import Logo from './Logo'
-import {HomeIcon, NotificationsIcon, SetingsIcon} from '../icons/Index'
+import {HomeIcon, LogoutIcon, NotificationsIcon, SetingsIcon} from '../icons/Index'
 
 type SidebarProps = {
     children?: any
@@ -8,14 +8,17 @@ type SidebarProps = {
 
 export default function Sidebar(props: SidebarProps){
     return (
-        <aside className="p-3">
+        <aside className="flex flex-col">
             <div className="h-20 w-25 bg-gradient-to-r from-indigo-500 to-purple-800 flex flex-col justify-center items-center">
                 <Logo></Logo>
             </div>
-            <ul>
+            <ul className="flex-1">
                 <SidebarItem url='/' text="Home" icon={HomeIcon}/>
                 <SidebarItem url='/setings' text="Setings" icon={SetingsIcon}/>
                 <SidebarItem url='/notifications' text="Notifications" icon={NotificationsIcon}/>
+            </ul>
+            <ul>
+                <SidebarItem cls='text-red-400 hover:bg-red-600 hover:text-white' onClick={e => {}} text="Logout" icon={LogoutIcon}/>
             </ul>
         </aside>
     )
--- src/components/template/SidebarItem.tsx
+++ src/components/template/SidebarItem.tsx
@@ -1,20 +1,33 @@
 import Link from "next/link"
 
 type SidebarItemProps = {
-    url: string
+    cls?: string
+    url?: string
+    onClick?: (e: any) => void
     text: string
     icon: any
 }
 
 export default function SidebarItem(props: SidebarItemProps){
-    return (
-        <li className='py-3 hover:bg-gray-100'>
-            <Link href={props.url}>
-                <a className='flex flex-col items-center justify-center p-2'>
+
+    function renderLink(){
+        return (
+                <a className={`flex flex-col items-center justify-center p-3 py-5 text-gray-600 ${props.cls}`}>
                     {props.icon}
-                    <span className='text-sm font-light text-gray-600'>{props.text}</span>
+                    <span className='text-sm font-light '>{props.text}</span>
                 </a>
-            </Link>
+        )
+    }
+
+    return (
+        <li onClick={props.onClick} className={`hover:bg-gray-100 cursor-pointer ${props.cls}`}>
+            {props.url ? (
+                <Link href={props.url}>
+                    {renderLink()}
+                </Link>
+            ) : (
+                renderLink()
+            )}
         </li>
     )
 }
