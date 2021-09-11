a302a6a12efd523230d75758044af05effc1e695
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-09-11 10:30:04 -0300

    Sidebar developed

--- src/components/template/Sidebar.tsx
+++ src/components/template/Sidebar.tsx
@@ -1,9 +1,18 @@
+import SidebarItem from './SidebarItem'
+import {HomeIcon, NotificationsIcon, SetingsIcon} from '../icons/Index'
+
 type SidebarProps = {
     children?: any
 }
 
 export default function Sidebar(props: SidebarProps){
     return (
-        <aside></aside>
+        <aside className="p-3">
+            <ul>
+                <SidebarItem url='/' text="Home" icon={HomeIcon}/>
+                <SidebarItem url='/setings' text="Setings" icon={SetingsIcon}/>
+                <SidebarItem url='/notifications' text="Notifications" icon={NotificationsIcon}/>
+            </ul>
+        </aside>
     )
 }
--- src/components/template/SidebarItem.tsx
+++ src/components/template/SidebarItem.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 type SidebarItemProps = {
     url: string
     text: string
@@ -6,8 +8,13 @@ type SidebarItemProps = {
 
 export default function SidebarItem(props: SidebarItemProps){
     return (
-        <li className='py-4'>
-            {props.icon}
+        <li className='py-3 hover:bg-gray-100'>
+            <Link href={props.url}>
+                <a className='flex flex-col items-center justify-center p-2'>
+                    {props.icon}
+                    <span className='text-sm font-light text-gray-600'>{props.text}</span>
+                </a>
+            </Link>
         </li>
     )
 }
