57e12731c16fa5af89f65a3f706b73983e0590fe
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-09-13 08:49:17 -0300

    login screen visual worked

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -21,9 +21,16 @@ export default function Login(){
     }
 
     return (
-        <div className="flex flex-col justify-center items-center h-screen">
-            <div className='w-1/3'>
-                <h1 className='text-xl font-bold mb-5'>
+        <div className="flex justify-center items-center h-screen">
+            <div className="hidden sm:block w-2/3">
+                <img src='https://images.unsplash.com/photo-1629670390087-fd9717429204?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTYzMTUzMTE5MQ&ixlib=rb-1.2.1&q=80&w=1080' alt='Login screen random image' className='h-screen w-full object-cover'/>
+            </div>
+            <div className='w-full sm:w-1/3 m-20'>
+                <div className="text-7xl font-bold text-center mb-20">
+                    <span className="text-pink-500">A</span>
+                    <span className='text-green-700'>R</span>
+                </div>
+                <h1 className='text-2xl font-bold mb-5'>
                     {mode === 'login' ? 'Sign in with your account' : 'Create your account'}
                 </h1>
                 <AuthInput type="email" label="Login:" value={email} onValueChange={setEmail} required/>
@@ -34,9 +41,19 @@ export default function Login(){
 
                 <hr className='my-6 border-gray-300 w-full'/>
 
-                <button onClick={submit} className='w-full bg-red-500 hover:bg-red-400 text-white rounded-lg px-4 py-3'>
+                <button onClick={submit} className='w-full bg-red-500 hover:bg-red-400 text-white rounded-lg px-4 py-3 mb-4'>
                     <FontAwesomeIcon icon={faGoogle} /> Sign in with Google
                 </button>
+
+                {mode === 'login' ? (
+                    <p>
+                        New? <a className='text-blue-500 hover:text-blue-700 cursor-pointer' onClick={() => setMode('register')}>Click here</a> to create your free account.
+                    </p>
+                ) : (
+                    <p>
+                        Already have an account? <a className='text-blue-500 hover:text-blue-700 cursor-pointer' onClick={() => setMode('login')}>Click here</a> to login.
+                    </p>
+                )}
             </div>
         </div>
     )
