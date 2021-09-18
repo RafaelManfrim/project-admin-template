0cf8463e2f4c855531f682c6db22d5e88f1a4c39
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-09-18 13:01:31 -0300

    localStorage theme save

--- src/data/context/AppContext.tsx
+++ src/data/context/AppContext.tsx
@@ -1,7 +1,7 @@
-import { createContext, useState, useContext } from 'react'
+import { createContext, useState, useEffect, useContext } from 'react'
 
 type AppContextProps = {
-    theme: 'dark' | ''
+    theme?: 'dark' | ''
     switchTheme: () => void
 }
 
@@ -18,9 +18,17 @@ export function AppProvider(props: AppProviderProps){
 
     const useAppContext = useContext(AppContext)
     const [theme, setTheme] = useState<any>('')
+
     function switchTheme(){
-        setTheme(theme === '' ? 'dark' : '')
+        const newTheme = theme === '' ? 'dark' : ''
+        setTheme(newTheme)
+        localStorage.setItem('theme', newTheme)
     }
+
+    useEffect(() => {
+        const themeSaved = localStorage.getItem('theme')
+        setTheme(themeSaved)
+    }, [])
 
     return (
         <AppContext.Provider value={{theme, switchTheme}}>
