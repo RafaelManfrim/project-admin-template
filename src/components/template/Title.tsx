fb5a870b2cafa6718628ceb47cc4660ec1e4e324
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-09-18 13:01:50 -0300

    Subtitle position fixed

--- src/components/template/Title.tsx
+++ src/components/template/Title.tsx
@@ -6,9 +6,9 @@ type TitleProps = {
 
 export default function Title(props: TitleProps){
     return (
-        <>
+        <div className="flex flex-col">
             <h1 className="font-black text-3xl text-gray-900 dark:text-gray-200">{props.title}</h1>
             <h2 className="font-light text-md text-gray-600 dark:text-gray-400">{props.subtitle}</h2>
-        </>
+        </div>
     )
 }
